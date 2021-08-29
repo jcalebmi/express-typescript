c060b606398bff9a0e1fd6dfd48ac8da7a4b2d2e
Caleb Iuliano <calebiuliano@gmail.com>
2021-08-29 13:15:51 -0400

    added sessions

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,18 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined}
+}
+
+function requiredAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  } else {
+    res.status(403);
+    res.send('Not Permitted');
+  }
+}
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
@@ -20,10 +33,47 @@ router.get('/login', (req: Request, res: Response) => {
   )
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
   const {email, password} = req.body;
 
-  res.send(email + password);
+  if (email && password && email === 'hi@hi.com' && password === 'password') {
+    req.session = { loggedIn: true };
+    res.redirect('/')
+  } else {
+    res.send("Invalid");
+  }
 });
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(
+      `
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+      `
+    )
+  } else {
+    res.send(
+      `
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+      `
+    )
+  }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+})
+
+router.get('/protected', requiredAuth, (req: Request, res: Response) => {
+  res.send('Welcome')
+})
+
 
 export { router };
