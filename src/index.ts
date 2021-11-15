291bd14dd2d179b51095962cc8388ac728b9e9f4
Caleb Iuliano <calebiuliano@gmail.com>
2021-11-15 17:21:05 -0500

    added decorators

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({keys: ['laskdjf']}));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 
 app.listen(3000, () => {
